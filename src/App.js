5252f46b7b88cc2807186a013a84fe5732e1776f
shukl kashyap <shuklakashyap777777@gmail.com>
2021-01-13 11:02:49 +0530

    Header logo

--- src/App.js
+++ src/App.js
@@ -4,7 +4,15 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h4>Let's get start</h4>
+     
+      <div className="header">
+      <img 
+      className="header_Img"
+      src="https://upload.wikimedia.org/wikipedia/commons/0/06/%C4%B0nstagram-Profilime-Kim-Bakt%C4%B1-1.png"
+      alt=""
+      />
+      </div>
+      <h1>Let's get start</h1>
     </div>
   );
 }
