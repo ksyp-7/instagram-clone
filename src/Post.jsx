0e488a5921b20dbc28ee4605f8b6ee129acf7cd4
shukl kashyap <shuklakashyap777777@gmail.com>
2021-01-13 11:59:12 +0530

    added dummy data

--- src/Post.jsx
+++ src/Post.jsx
@@ -1,13 +1,24 @@
-import React from 'react'
+import React from 'react';
+import { } from "./Post.css";
+import Avatar from '@material-ui/core/Avatar';
 
 function Post() {
     return (
-        <div>
-            <h3>Username</h3>
+        <div className="post">
+            <div className="post_header">
+                <Avatar
+                    className="post_avatar"
+                    alt="S"
+                    src="https://picsum.photos/200/300" />
+                <h3>Username</h3>
+            </div>
+
             {/*header contains avatar nd username */}
-            <img src="https://upload.wikimedia.org/wikipedia/commons/0/06/İnstagram-Profilime-Kim-Baktı-1.png" />
+            <img className="Post_img" src="https://upload.wikimedia.org/wikipedia/commons/0/06/İnstagram-Profilime-Kim-Baktı-1.png" />
             {/*Image section */}
-            <h4>Username : caption</h4>
+            <h4 className="post_text">
+                <strong>ksyp.7  </strong>
+                this_is_the_caption.</h4>
             {/*username nd cation implement section*/}
 
         </div>
