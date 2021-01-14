7d3c09e8516a090f03c1c647b7d586e10d957b45
shukl kashyap <shuklakashyap777777@gmail.com>
2021-01-14 12:19:27 +0530

    Implemented useEffect
    now data is coming from db function implemented in firebase.jsx

--- src/App.js
+++ src/App.js
@@ -1,59 +1,36 @@
-import React, {useState} from 'react';
+import React, { useState, useEffect } from 'react';
 import Post from './Post';
+import {db} from './firebase.jsx';
 import './App.css';
 
 function App() {
-  const [posts,setPosts] =  useState([
-    {
-      username: "ksyp.7",
-      caption: "This is State",
-      imgUrl:"https://picsum.photos/350/201"
-    },
-    {
-      imgUrl:"https://picsum.photos/351/200",
-       username:"b_7",
-       caption:"ab abcd abcdefg abcgedhd dhksa jsljnv sdnkdd"
-    },
-    {
-      imgUrl:"https://picsum.photos/352/200",
-      username:"luci",
-      caption:"ab abcd abcdefg abcgedhd dhksa jsljnv sdnkdd"
-    },
-    {
-      imgUrl:"https://picsum.photos/350/203",
-       username:"raj",
-       caption:"ab abcd abcdefg abcgedhd dhksa jsljnv sdnkdd"
-    },
-    {
-      imgUrl:"https://picsum.photos/350/202",
-       username:"yash_1",
-       caption:"ab abcd abcdefg abcgedhd dhksa jsljnv sdnkdd"
-    }
-    
-  ]);
+  const [posts, setPosts] = useState([]);
+  //useEfect -> Runs a piece of code based on a specific condition
+  useEffect(() => {
+    //this is where the code runs
+    db.collection('posts').onSnapshot(snapshot => {
+      //every time a new post is added, this code fires...
+      setPosts(snapshot.docs.map(doc => doc.data()))
+    })
+  }, []);
   return (
     <div className="App">
-     
+
       <div className="header">
-      <img 
-      className="header_Img"
-      src="https://upload.wikimedia.org/wikipedia/commons/0/06/%C4%B0nstagram-Profilime-Kim-Bakt%C4%B1-1.png"
-      alt=""
-      />
+        <img
+          className="header_Img"
+          src="https://upload.wikimedia.org/wikipedia/commons/0/06/%C4%B0nstagram-Profilime-Kim-Bakt%C4%B1-1.png"
+          alt=""
+        />
       </div>
-    {
-      posts.map(post =>(
-        <Post username={post.username}
-        caption={post.caption}
-         imgUrl={post.imgUrl}
-         />
-      ))
-    }
-      <Post
-       imgUrl="https://picsum.photos/350/200"
-       username="i.m._k7"
-       caption="ab abcd abcdefg abcgedhd dhksa jsljnv sdnkdd"
-       />
+      {
+        posts.map(post => (
+          <Post username={post.username}
+            caption={post.caption}
+            imgUrl={post.imgUrl}
+          />
+        ))
+      }
     </div>
   );
 }
