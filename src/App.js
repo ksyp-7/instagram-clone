a58cafa0c02462821d8aa4d26379799d2020fd6e
shukl kashyap <shuklakashyap777777@gmail.com>
2021-01-14 10:42:23 +0530

    used map method to render data

--- src/App.js
+++ src/App.js
@@ -1,7 +1,36 @@
+import React, {useState} from 'react';
 import Post from './Post';
 import './App.css';
 
 function App() {
+  const [posts,setPosts] =  useState([
+    {
+      username: "ksyp.7",
+      caption: "This is State",
+      imgUrl:"https://picsum.photos/350/201"
+    },
+    {
+      imgUrl:"https://picsum.photos/351/200",
+       username:"b_7",
+       caption:"ab abcd abcdefg abcgedhd dhksa jsljnv sdnkdd"
+    },
+    {
+      imgUrl:"https://picsum.photos/352/200",
+      username:"luci",
+      caption:"ab abcd abcdefg abcgedhd dhksa jsljnv sdnkdd"
+    },
+    {
+      imgUrl:"https://picsum.photos/350/203",
+       username:"raj",
+       caption:"ab abcd abcdefg abcgedhd dhksa jsljnv sdnkdd"
+    },
+    {
+      imgUrl:"https://picsum.photos/350/202",
+       username:"yash_1",
+       caption:"ab abcd abcdefg abcgedhd dhksa jsljnv sdnkdd"
+    }
+    
+  ]);
   return (
     <div className="App">
      
@@ -12,34 +41,17 @@ function App() {
       alt=""
       />
       </div>
+    {
+      posts.map(post =>(
+        <Post username={post.username}
+        caption={post.caption}
+         imgUrl={post.imgUrl}
+         />
+      ))
+    }
       <Post
        imgUrl="https://picsum.photos/350/200"
        username="i.m._k7"
-       caption="ab abcd abcdefg abcgedhd dhksa jsljnv sdnkdd"
-       />
-      <Post
-       imgUrl="https://picsum.photos/350/201"
-       username="ksyp.7"
-       caption="ab abcd abcdefg abcgedhd dhksa jsljnv sdnkdd"
-       />
-       <Post
-       imgUrl="https://picsum.photos/350/202"
-       username="yash_1"
-       caption="ab abcd abcdefg abcgedhd dhksa jsljnv sdnkdd"
-       />
-       <Post
-       imgUrl="https://picsum.photos/350/203"
-       username="raj"
-       caption="ab abcd abcdefg abcgedhd dhksa jsljnv sdnkdd"
-       />
-       <Post
-       imgUrl="https://picsum.photos/351/200"
-       username="b_7"
-       caption="ab abcd abcdefg abcgedhd dhksa jsljnv sdnkdd"
-       />
-       <Post
-       imgUrl="https://picsum.photos/352/200"
-       username="luci"
        caption="ab abcd abcdefg abcgedhd dhksa jsljnv sdnkdd"
        />
     </div>
