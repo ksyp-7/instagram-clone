27cae75dcb512b74290f596d0f1ab2cfac519285
shukl kashyap <shuklakashyap777777@gmail.com>
2021-01-20 10:17:42 +0530

    Post Componet
    post componet fully updted and linked with firebase..
    
    so we can render that uploaded image

--- src/ImageUpload.jsx
+++ src/ImageUpload.jsx
@@ -1,32 +1,69 @@
-import React,{useState} from 'react';
+import React, { useState } from 'react';
 import { Button, Input } from '@material-ui/core';
+import { storage, db } from './firebase';
+import firebase from 'firebase';
 
-function ImageUpload() {
-    const [caption,setCaption] = useState('');
-    const [progress,setProgress] = useState('');
-    const [image,setImage] = useState('');
+function ImageUpload({ username }) {
+    const [caption, setCaption] = useState('');
+    const [progress, setProgress] = useState('');
+    const [image, setImage] = useState('');
 
     const handelChange = (e) => {
-        if(e.target.files[0]){
+        if (e.target.files[0]) {
             setImage(e.target.files[0]);
         }
     };
 
     const handelUpload = () => {
-
-    }
+        const uploadTask = storage.ref(`images/${image.name}`).put(image);
+        uploadTask.on(
+            "state_changed",
+            (snapshot) => {
+                const progress = Math.round(
+                    (snapshot.bytesTransferred / snapshot.totalBytes * 100)
+                );
+                setProgress(progress);
+            },
+            (error) => {
+                console.log(error);
+                alert(error.message);
+            },
+            () => {
+                storage
+                    .ref("images")
+                    .child(image.name)
+                    .getDownloadURL()
+                    .then(url => {
+                        db.collection("posts").add({
+                            timestap: firebase.firestore.FieldValue.serverTimestamp(),
+                            caption: caption,
+                            imgUrl: url,
+                            username: username
+                        });
+                        setProgress(0);
+                        setCaption("");
+                        setImage(null);
+                    });
+            }
+        );
+    };
     return (
         <div>
-            <input
+            <progress
+                value={progress}
+                max="100"
+            />
+            <Input
                 type="text"
                 placeholder='Enter a caption ....'
                 onChange={event => setCaption(event.target.value)}
             />
-            <input
+            <br />
+            <Input
                 type="file"
                 onChange={handelChange}
             />
-            <Button onClick={handelChange}>
+            <Button onClick={handelUpload}>
                 Upload
         </Button>
 
